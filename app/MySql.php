a5aceedc5bd32ee894cc78eed652ecb637fc1bd2
Saúl Edgardo Soto Ruelas <19030073@uadeo.mx>
2022-04-04 12:30:28 -0600

    C13

--- app/MySql.php
+++ app/MySql.php
@@ -1,3 +1,12 @@
 <?php
-    //TODO: Conexion de MYSQL
+    use \mysqli;
+    class mysql extends \mysqli{
+        protected $data = [
+            'host' => '127.0.0.1',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'uadeo',
+            'port' => 3306
+        ];
+    }
 ?>
